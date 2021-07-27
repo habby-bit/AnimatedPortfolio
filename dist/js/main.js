6b40b578c8aacb96db292a2d180e0fded3c6bd67
Habby Olusesi <habbyolu@gmail.com>
2021-07-27 18:13:54 -0400

    home animations invisible

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menuNav= document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const homeContent = document.querySelector('#home')
 
 // Set Initial State of Menu
 let showMenu = false;
@@ -12,6 +13,7 @@ menuBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     if(!showMenu) {
+        homeContent.classList.add('show')
         menuBtn.classList.add('close');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
@@ -19,17 +21,17 @@ function toggleMenu() {
         navItems.forEach(item => {
             item.classList.add('show')
         })
-
         // Set Menu State
         showMenu = true;
     }
     else {
+        homeContent.classList.remove('show')
         menuBtn.classList.remove('close');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         menu.classList.remove('show');
         navItems.forEach(item => {
-            item.classList.remove('show')
+            item.classList.remove('show')    
         })
 
         // Set Menu State
